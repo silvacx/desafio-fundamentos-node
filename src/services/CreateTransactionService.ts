6677f2a09ef07a8a4307ac9630055e48548e7887
Camila Silva <milanasc12@gmail.com>
2020-04-25 20:51:53 -0300

    Entrega desafio

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,6 +1,12 @@
 import TransactionsRepository from '../repositories/TransactionsRepository';
 import Transaction from '../models/Transaction';
 
+interface RequestDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class CreateTransactionService {
   private transactionsRepository: TransactionsRepository;
 
@@ -8,8 +14,26 @@ class CreateTransactionService {
     this.transactionsRepository = transactionsRepository;
   }
 
-  public execute(): Transaction {
-    // TODO
+  public execute({ title, value, type }: RequestDTO): Transaction {
+    const transactionType = type;
+    const { total } = this.transactionsRepository.getBalance();
+
+    // if(!["income", "outcom"].includes(type))
+    if (transactionType !== 'income' && transactionType !== 'outcome') {
+      throw Error('You do not have enout balance');
+    }
+
+    if (type === 'outcome' && total < value) {
+      throw Error('Error creating this transaction');
+    }
+
+    const transaction = this.transactionsRepository.create({
+      title,
+      value,
+      type,
+    });
+
+    return transaction;
   }
 }
 
